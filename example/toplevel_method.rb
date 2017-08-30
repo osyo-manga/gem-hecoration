4b22b17899deee68076ea76a00283fa2955243af
manga_osyo <manga.osyo@gmail.com>
2017-08-30 17:43:47 +0900

    Update.

--- example/toplevel_method.rb
+++ example/toplevel_method.rb
@@ -1,36 +1,10 @@
 require "hecoration"
 
-extend Hecoration::Decoratable
-
-def deco
-	# Wrapping block
-	decorator { |f|
-		proc {
-			puts "--- start ---"
-			# Call original method
-			f.bind(self).call
-			puts "--- end ---"
-		}
-	}
-end
-
-# Wrap method when next defined method.
-# Must be call #rebind(Object)
-+deco.rebind(Object)
-def hello
-	puts "hello"
-end
-
-hello
-# output:
-# --- start ---
-# hello
-# --- end ---
-
+# Using Object#decorate_singleton_method
+using Hecoration
 
 def print_args
-	# With arguments.
-	decorator { |f|
+	proc { |f|
 		proc { |*args|
 			puts "args:#{args}"
 			f.bind(self).call(*args)
@@ -38,10 +12,11 @@ def print_args
 	}
 end
 
-+print_args.rebind(Object)
 def plus a, b
 	a + b
 end
+# Decorate method
+decorate_singleton_method(:plus, &print_args)
 
 puts plus 1, 2
 # output:
